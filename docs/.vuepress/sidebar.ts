c7c61eca96aec466cd0aac43d5edb99baf3ab2ee
Scaxlibur <51772892+Scaxlibur@users.noreply.github.com>
2024-11-09 23:47:31 +0800

    更新 sidebar.ts

--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -43,29 +43,12 @@ export default sidebar({
       children: "structure",
     },
     {
-      text: "NAS、服务器和软路由",
-      icon: "fa6-solid:bars-staggered",
-      prefix: "/",
-      children: [
-        //"/NASServerRouter/",
-        {
-          text: "NAS",
-          icon: "fa6-brands:chrome",
-          link: "NASServerRouter/NAS/",
-        },
-        /*
-        {
-          text: "服务器",
-          icon: "fa6-brands:chrome",
-          link: "NASServerRouter/服务器/",
-        },
-        */
-        {
-          text: "软路由",
-          icon: "fa6-brands:chrome",
-          link: "NASServerRouter/router/",
-        },
-  ],
+      text: "🔧 NAS、服务器和路由器",
+      icon: "",
+      prefix: "/NASServerRouter/",
+      collapsible: true,
+      children: "structure",
+    },
   // 专题区（独立侧边栏）
   //"/NASServerRouter/NAS/": "structure",
 });
