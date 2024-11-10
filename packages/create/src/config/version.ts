61a6616c4aa91d02acf7aea4ac46e88e7aa67157
Scaxlibur <scaxlibur@gmail.com>
2024-11-10 15:07:56 +0800

    重来

--- packages/create/src/config/version.ts
+++
@@ -1,5 +0,0 @@
-import { createRequire } from "node:module";
-
-export const { version } = createRequire(import.meta.url)(
-  "create-vuepress-theme-hope/package.json",
-) as Record<string, unknown> & { version: string };
