afd2990707a8314044c6dee2708f462cb6272452
Scaxlibur <scaxlibur@gmail.com>
2026-04-28 13:53:30 +0800

    Revert "chore: upgrade theme and prune vocab pages"
    This reverts commit 151c54f61ce9f343356a4fe66d9ea717ab852265.

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,7 +2,7 @@ import SocialLink from "./components/SocialLink.js";
 import { defineClientConfig } from "vuepress/client";
 
 export default defineClientConfig({
-  enhance(ctx) {
-    ctx.app.component("SocialLink", SocialLink);
+  enhance: ({ app }) => {
+    app.component("SocialLink", SocialLink);
   },
 });
