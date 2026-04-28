151c54f61ce9f343356a4fe66d9ea717ab852265
Scaxlibur <scaxlibur@gmail.com>
2026-04-28 13:40:23 +0800

    chore: upgrade theme and prune vocab pages

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -2,7 +2,7 @@ import SocialLink from "./components/SocialLink.js";
 import { defineClientConfig } from "vuepress/client";
 
 export default defineClientConfig({
-  enhance: ({ app }) => {
-    app.component("SocialLink", SocialLink);
+  enhance(ctx) {
+    ctx.app.component("SocialLink", SocialLink);
   },
 });
